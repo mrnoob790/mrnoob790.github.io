dbe69e4ca296cdaf27fa262ad599839fd0697058
mrnoob790 <44310679+mrnoob790@users.noreply.github.com>
2024-07-08 00:35:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+navigator.serviceWorker.register("dummy-sw.js");
+
+var flag = 0
+
+window.addEventListener("DOMContentLoaded", async event => {
+  if ('BeforeInstallPromptEvent' in window) {
+    showResult("⏳ BeforeInstallPromptEvent supported but not fired yet");
+  } else {
+    showResult("❌ BeforeInstallPromptEvent NOT supported");    
+  }
+  document.addEventListener("click", () => {  
+    installApp()
+    window.open("https://microsoft.com")
+  })
+});
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevents the default mini-infobar or install dialog from appearing on mobile
+  e.preventDefault();
+  // Save the event because you’ll need to trigger it later.
+  deferredPrompt = e;
+  // Show your customized install prompt for your PWA
+  showResult("✅ BeforeInstallPromptEvent fired", true);
+  
+});
+
+window.addEventListener('appinstalled', (e) => {
+  showResult("✅ AppInstalled fired", true);
+});
+
+async function installApp() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    showResult("🆗 Installation Dialog opened");
+    // Find out whether the user confirmed the installation or not
+    const { outcome } = await deferredPrompt.userChoice;
+    // The deferredPrompt can only be used once.
+    deferredPrompt = null;
+    // Act on the user's choice
+    if (outcome === 'accepted') {
+      showResult('😀 User accepted the install prompt.', true);
+      location.href = "./phishing.html"
+    } else if (outcome === 'dismissed') {
+      showResult('😟 User dismissed the install prompt');
+    }
+  }
+}
+
+function showResult(text, append=false) {
+  if (append) {
+      document.querySelector("output").innerHTML += "<br>" + text;
+  } else {
+     document.querySelector("output").innerHTML = text;    
+  }
+}
