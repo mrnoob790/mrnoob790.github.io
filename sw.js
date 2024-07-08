91ed013e468907d4ffe121b198585a87a4aa4ce1
mrnoob790 <44310679+mrnoob790@users.noreply.github.com>
2024-07-08 08:18:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener("install", (event) => {                      });
+self.addEventListener("install", (event) => {  self.skipWaiting(); });
 self.addEventListener("fetch",   (event) => {                      });
