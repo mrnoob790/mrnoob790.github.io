8a025ec1d20b3c026910020848bb45180a547086
mrnoob790 <44310679+mrnoob790@users.noreply.github.com>
2024-07-25 12:11:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var popupLink = document.getElementById('popup_link');
-    
-    popupLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        // Open a new window with google.com
-        var popupWindow = window.open('https://google.com', '_blank');
-        
-        // Set the title of the new window
-        popupWindow.document.title = 'google.com';
-    });
+window.addEventListener("load", function onLoad(event) {
+    var popupLink = document.getElementById("popup_link");
+    popupLink.onclick = openPopupWindow;
 });
+
+function openPopupWindow() {
+    var newWindow = window.open("javascript:''", "_blank");
+
+    // Set the title of the new popup window
+    newWindow.document.title = "Google";
+
+    var iframe = newWindow.document.createElement("iframe");
+    iframe.src = "http://neverssl.com/";
+
+    newWindow.document.body.appendChild(iframe);
+}
